e88d0bf13559fe749aba150d54e7bd77e49cee26
Pfeter <peter.foldvary@gmail.com>
2016-06-06 09:30:01 +0200

    Add the solution of the first excercise

--- 1.py
+++ 1.py
@@ -1,3 +1,11 @@
 # Create a function that takes a list as a parameter,
 # and returns a new list with all it's element value doubled.
 # It should raise an error if the parameter is not a list
+def doubled(input_list):
+    if type(input_list) is list:
+        output = []
+        for i in input_list:
+            output.append(i * 2)
+        return output
+    else:
+        raise
