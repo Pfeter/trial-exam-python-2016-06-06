a7edada83153e4af65b37a099b0ebbd891bb1a15
Pfeter <peter.foldvary@gmail.com>
2016-06-06 09:45:13 +0200

    Add the solution of the third excercise

--- 3.py
+++ 3.py
@@ -2,3 +2,18 @@
 # It should take it's three sizes as constructor parameters (numbers)
 # It should have a method called `getSurface` that returns the cuboid's surface
 # It should have a method called `getVolume` that returns the cuboid's volume
+class Cuboid():
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def getSurface(self):
+        return self.x * self.y * 2 + self.y * self.z * 2 + self.z * self.x *2
+
+    def getVolume(self):
+        return self.x * self.y * self.z
+
+# kocka = Cuboid(2,2,2)
+# print(kocka.getSurface())
+# print(kocka.getVolume())
