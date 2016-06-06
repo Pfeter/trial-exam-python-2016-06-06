5e9b899e626151559973b5b7150e880dd243471d
Pfeter <peter.foldvary@gmail.com>
2016-06-06 10:08:09 +0200

    Add the solution of the fourth excercise

---
+++ test_4.py
@@ -0,0 +1,15 @@
+import unittest
+from exam4 import greeter
+
+class TestAnagrammAndCountLetters(unittest.TestCase):
+
+    def test_working_name_correct_result(self):
+        result = greeter('Peter')
+        self.assertEqual('Hello, Peter!', result)
+
+    def test_empty_name_failed_result(self):
+        result = greeter('')
+        self.assertEqual('Hello, Mr Nobody!', result)
+
+if __name__ == '__main__':
+    unittest.main()
